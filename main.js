177af80dd35d5be86f0766f2b73131b8ebc6fac4
HuangJM7 <jm_william@qq.com>
2019-11-28 21:26:49 +0800

    教育背景

--- main.js
+++ main.js
@@ -2,5 +2,6 @@
 
 window.addEventListener("load", function () {
     document.querySelector(".photoNumber").innerText = `手机|微信:${atob("MTgyMDc3MjcxOTA")}`
+    document.querySelector(".qqNumber").innerText = `QQ:${atob("Mjc0Njg1MTc4")}`
 })
 
